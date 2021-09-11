bd4be83c3bc79cac1d2806a04a50f20cd01bfc43
kimdaniels1999 <86605048+kimdaniels1999@users.noreply.github.com>
2021-09-11 12:55:09 +1000

    11.09.2021 1255

--- crowdfunding/projects/urls.py
+++ crowdfunding/projects/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 
-urlpatterns = [path('projects/', views.ProjectList.as_view()),
-    path('projects/<int:pk>/', views.ProjectDetail.as_view())
+urlpatterns = [
+    path('projects/', views.ProjectList.as_view()),
+    path('projects/<int:pk>/', views.ProjectDetail.as_view()),
+    path('pledges/', views.PledgeList.as_view())
 
 ]
 
