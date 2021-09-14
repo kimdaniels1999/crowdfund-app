78c785fedf638c363d5458149a587426ea9b95fd
kimdaniels1999 <86605048+kimdaniels1999@users.noreply.github.com>
2021-09-14 20:13:12 +1000

    14.09.2021

---
+++ crowdfunding/users/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+# Create your models here.
+
+class CustomUser(AbstractUser):
+    pass
+
+    def _str_(self):
+        return self.username
+
+
