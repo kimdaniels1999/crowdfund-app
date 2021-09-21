bb398dd1b2c3ab79e9e0d79b282dcad3d2bb1685
kimdaniels1999 <86605048+kimdaniels1999@users.noreply.github.com>
2021-09-21 18:54:21 +1000

    21.09.2021

--- crowdfunding/crowdfunding/settings.py
+++ crowdfunding/crowdfunding/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +22,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-v-ru*b^od$c8)b(uu(odu=t%(5&(43rhoq5+9@vy1yx1-r#d!m'
+# SECRET_KEY = 'django-insecure-v-ru*b^od$c8)b(uu(odu=t%(5&(43rhoq5+9@vy1yx1-r#d!m'
+
+SECRET_KEY = os.environ.get(
+    'DJANGO_SECRET_KEY',
+    'django-insecure-v-ru*b^od$c8)b(uu(odu=t%(5&(43rhoq5+9@vy1yx1-r#d!m'
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+
+DEBUG = os.environ.get('DJANGO_DEBUG') != 'FALSE'
 
 ALLOWED_HOSTS = []
 
@@ -89,6 +98,9 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 
 # Password validation
