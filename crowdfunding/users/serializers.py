3c1bec58fe2f349bb1ddc3ebeca1b1a52c6bf7db
kimdaniels1999 <86605048+kimdaniels1999@users.noreply.github.com>
2021-09-24 22:30:34 +1000

    changepassword

--- crowdfunding/users/serializers.py
+++ crowdfunding/users/serializers.py
@@ -1,14 +1,22 @@
 from rest_framework import serializers
 from .models import CustomUser
+from django.contrib.auth.models import User
 
 class CustomUserSerializer(serializers.Serializer):
     id = serializers.ReadOnlyField()
     username = serializers.CharField(max_length=200)
     email = serializers.CharField(max_length=None, min_length=None, allow_blank=False)
     #password = serializers.CharField(max_length=8)
-    #user_photo = serializers.ImageField(max_length=0, use_url=True)
-    #online_profile = serializers.URLField(max_length=0, use_url=True)
 
     def create(self, validated_data):
         return CustomUser.objects.create(**validated_data)
 
+class ChangePasswordSerializer(serializers.Serializer):
+    model = User
+
+    #Serializer for password change endpoint
+
+    old_password = serializers.CharField(required=True)
+    new_password = serializers.CharField(required=True)
+
+    
--- crowdfunding/users/views.py
+++ crowdfunding/users/views.py
@@ -2,8 +2,12 @@ from django.http import Http404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework import generics
+from django.contrib.auth.models import User
 from .models import CustomUser
 from .serializers import CustomUserSerializer
+from .serializers import ChangePasswordSerializer
+from rest_framework.permissions import IsAuthenticated
 
 class CustomUserList(APIView):
     
@@ -30,3 +34,38 @@ class CustomUserDetail(APIView):
         user = self.get_object(pk)
         serializer = CustomUserSerializer(user)
         return Response(serializer.data)
+
+class ChangePasswordView(generics.UpdateAPIView):
+    """
+    An endpoint for changing password.
+    """
+    serializer_class = ChangePasswordSerializer
+    model = User
+    permission_classes = (IsAuthenticated,)
+
+    def get_object(self, queryset=None):
+        obj = self.request.user
+        return obj
+
+    def update(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        serializer = self.get_serializer(data=request.data)
+
+        if serializer.is_valid():
+            # Check old password
+            if not self.object.check_password(serializer.data.get("old_password")):
+                return Response({"old_password": ["Wrong password."]}, status=status.HTTP_400_BAD_REQUEST)
+            # set_password also hashes the password that the user will get
+            self.object.set_password(serializer.data.get("new_password"))
+            self.object.save()
+            response = {
+                'status': 'success',
+                'code': status.HTTP_200_OK,
+                'message': 'Password updated successfully',
+                'data': []
+            }
+
+            return Response(response)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
