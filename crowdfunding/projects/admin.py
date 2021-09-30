f51e7a3b04ba6fbd1a9905f897215f8399a804cb
kimdaniels1999 <86605048+kimdaniels1999@users.noreply.github.com>
2021-09-30 23:43:41 +1000

    Admin

--- crowdfunding/projects/admin.py
+++ crowdfunding/projects/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Project, Pledge
 
 # Register your models here.
+
+class ProjectAdmin(admin.ModelAdmin):
+    list_filter = ("owner", "is_open")
+    list_display = ("title", "description", "goal", "date_created")
+admin.site.register(Project, ProjectAdmin)
+
+class PledgeAdmin(admin.ModelAdmin):
+    list_display = ("amount", "project", "supporter")
+    list_filter = ("project", "anonymous")
+admin.site.register(Pledge, PledgeAdmin)
+
